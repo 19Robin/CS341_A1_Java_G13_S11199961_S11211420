d552334e3abb93de6562b42922ab375c4d2495f0
Sam Robin <Samuelrobin156@gmail.com>
2024-09-29 13:06:29 +1200

    CalcInterestTest - Question 1 Task 2

--- CalcInterestTest/CalcInterestTest.java
+++ CalcInterestTest/CalcInterestTest.java
@@ -6,18 +6,32 @@ import org.junit.Test;
 
 public class CalcInterestTest {
 
+    // Test for Home loan under normal conditions
     @Test
-    public void testComputeLoanInterest_Home_Loan_5Years() {
-        assertEquals(40000.0, CalcInterest.computeLoanInterest(100000, 5, 1), 0.001);
+    public void testHomeLoanBelow100K5Years() {
+        assertEquals(4000.0f, CalcInterest.computeLoanInterest(50000, 5, 1), 0.01f);
     }
 
     @Test
-    public void testComputeLoanInterest_Property_Loan_11Years() {
-        assertEquals(385000.0, CalcInterest.computeLoanInterest(500000, 11, 2), 0.001);
+    public void testHomeLoanBetween100KAnd500K10Years() {
+        assertEquals(19500.0f, CalcInterest.computeLoanInterest(300000, 8, 1), 0.01f);
     }
 
+    // Test for Property loan under normal conditions
+    @Test
+    public void testPropertyLoanAbove500K11Years() {
+        assertEquals(35000.0f, CalcInterest.computeLoanInterest(500000, 12, 2), 0.01f);
+    }
+
+    // Test for Invalid loan type
     @Test
     public void testInvalidLoanType() {
-        assertEquals(-1, CalcInterest.computeLoanInterest(100000, 5, 3), 0.001);
+        assertEquals(-1, CalcInterest.computeLoanInterest(50000, 5, 3), 0.01f);
+    }
+
+    // Test for edge case
+    @Test
+    public void testHomeLoanUpperBoundary() {
+        assertEquals(32500.0f, CalcInterest.computeLoanInterest(500000, 11, 1), 0.01f);
     }
 }
